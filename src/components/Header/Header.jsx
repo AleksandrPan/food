f67495f06c173bf2f720a7a96a0c1dacd4ccba09
aleks <panin-a@inbox.ru>
2023-01-26 20:43:01 +0300

    fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,18 +4,18 @@ import './style.css';
 
 function Header({children, user, onUpdateUser}) {
 
-  const handleClickButtonEdit = (e)=> {
-    e.preventDefault();
-    onUpdateUser({name: 'Алекс', about: 'Студент'})
-  }
+  // const handleClickButtonEdit = (e)=> {
+  //   e.preventDefault();
+  //   onUpdateUser({name: 'Алекс', about: 'Студент'})
+  // }
 
   return (
     <header className='header'>
       <div className="container">
-        {user?.email && <span>{user?.email}</span>}
-        {user?.name && <span>{user?.name}</span>}
+        {/* {user?.email && <span>{user?.email}</span>}
+        {user?.name && <span>{user?.name}</span>} */}
 
-        <button className='btn' onClick={handleClickButtonEdit}>Изменить</button>
+        {/* <button className='btn' >Изменить</button> */}
         <div className="header__wrapper">
           {children}
         </div>
